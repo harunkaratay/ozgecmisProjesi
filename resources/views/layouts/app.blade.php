2f20708095ea5efc9396c41687730718c4a060e3
harunkaratay <harunkaratayy@outlook.com>
2025-05-09 01:39:51 +0300

    api eklendi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,8 +14,10 @@
     <link href="{{asset('panel/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
 
     <!-- Custom fonts for this template -->
-    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:100,200,300,400,500,600,700,800,900" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:100,200,300,400,500,600,700,800,900"
+          rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i"
+          rel="stylesheet">
     <link href="{{asset('panel/vendor/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
     <link href="{{asset('panel/vendor/devicons/css/devicons.min.css" rel="stylesheet')}}">
     <link href="{{asset('panel/vendor/simple-line-icons/css/simple-line-icons.css')}}" rel="stylesheet">
@@ -31,10 +33,12 @@
     <a class="navbar-brand js-scroll-trigger" href="#page-top">
         <span class="d-block d-lg-none">Start Bootstrap</span>
         <span class="d-none d-lg-block">
-          <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="{{asset('panel/img/kullan.jpeg')}}" alt="">
+          <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="{{asset('panel/img/kullan.jpeg')}}"
+               alt="">
         </span>
     </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -48,6 +52,13 @@
         </ul>
     </div>
 </nav>
+<!-- Hava Durumu -->
+<div id="weather-info"
+     style="position: fixed; top: 10px; right: 10px; z-index: 1050; background-color: rgba(255,255,255,0.8); padding: 10px; border-radius: 8px; box-shadow: 0 0 5px rgba(0,0,0,0.2);">
+    <p>Hava Durumu :</p>
+    <div id="weather-content">Yükleniyor...</div>
+</div>
+<!-- Hava Durumu Bitti -->
 
 <div class="container-fluid p-0">
 
@@ -131,6 +142,27 @@
 
 <!-- Custom scripts for this template -->
 <script src="{{asset('panel/js/resume.min.js')}}"></script>
+
+<!-- Hava durumu api -->
+<script>
+    async function fetchWeather() {
+        const apiKey = '0f6cd3e6244627207bc29ad8fc3b73eb';
+        const city = 'Elazig';
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=tr&appid=${apiKey}`;
+
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            const temp = data.main.temp;
+            const desc = data.weather[0].description;
+            document.getElementById('weather-content').innerHTML = `${city}: ${temp}°C, ${desc}`;
+        } catch (error) {
+            document.getElementById('weather-content').innerHTML = 'Veri alınamadı';
+            console.error(error);
+        }
+    }
+    fetchWeather();
+</script>
 
 </body>
 
