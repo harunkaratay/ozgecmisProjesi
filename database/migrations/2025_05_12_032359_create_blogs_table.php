1fa18200d872a5fa6e7b680ecf7f9ac49a2ee867
harunkaratay <harunkaratayy@outlook.com>
2025-05-14 15:57:59 +0300

    softdelete eklendi

--- database/migrations/2025_05_12_032359_create_blogs_table.php
+++ database/migrations/2025_05_12_032359_create_blogs_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('summary');
             $table->longText('content');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
