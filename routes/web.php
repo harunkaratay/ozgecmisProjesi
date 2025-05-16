4adad6d13069850ba79ff5ba8bad0ad62546b809
harunkaratay <harunkaratayy@outlook.com>
2025-05-16 12:26:04 +0300

    küçük stil düzeltmeleri yapıldı

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BlogController;
+
+//default
 Route::get('/', function () {
-    return view('home');
+    return view('hosgeldiniz');
 });
 
+
+//home route
 Route::get('/home', function () {
     return view('admin.home.cv');
 });
@@ -14,7 +18,7 @@ Route::get('/home', function () {
 //blog routes
 Route::get('/admin/blog/index', [BlogController::class, 'indexPage'])->name('blogIndex');
 Route::get('/admin/blog/show/{id}', [BlogController::class, 'showPage'])->name('blogShow');
-
+//admin görür
 Route::middleware(['auth'])->group(function () {
     Route::get('/admin/blog/create', [BlogController::class, 'createPage'])->name('blogCreate');
     Route::post('/admin/blog/add', [BlogController::class, 'addPage'])->name('blogAdd');
