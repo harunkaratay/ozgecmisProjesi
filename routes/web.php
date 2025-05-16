96cd0b0e955bb59736d384ba3f2b9545e1162c04
harunkaratay <harunkaratayy@outlook.com>
2025-05-16 03:02:13 +0300

    jetstream eklendi

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,13 @@ Route::get('/admin/blog/show/{id}', [BlogController::class, 'showPage'])->name('
 
 
 
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
