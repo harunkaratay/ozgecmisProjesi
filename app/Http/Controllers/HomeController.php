d55cb398993cb6cc255ed70e8d069df6926bb6a4
Muhammed Harun Karatay <harunkaratayy@outlook.com>
2025-05-21 16:41:37 +0300

    publicde blog ve category ilişkileri eklendi

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,17 +3,40 @@
 namespace App\Http\Controllers;
 
 use App\Models\Blog;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function indexBlog(){
-        $blogs = Blog::get();
-        return view('public.home.cv' , compact('blogs'));
+    public function indexBlog(Request $request)
+    {
+        $query = Blog::with('category');
+
+        if ($request->filled('category_id')) {
+            $category = Category::find($request->category_id);
+            if ($category) {
+                $categoryIds = $this->getAllCategoryIds($category);
+                $query->whereIn('category_id', $categoryIds);
+            }
+        }
+
+        $blogs = $query->get();
+        $categories = Category::with('childrenRecursive')->whereNull('parent_id')->get();
+
+        return view('public.home.cv', compact('blogs', 'categories'));
     }
 
     public function showBlog($id){
         $blogs = Blog::with('comments')->findOrFail($id);
         return view('public.home.show', compact('blogs'));
+    }
+
+    private function getAllCategoryIds($category)
+    {
+        $ids = [$category->id];
+        foreach ($category->children as $child) {
+            $ids = array_merge($ids, $this->getAllCategoryIds($child));
+        }
+        return $ids;
     }
 }
